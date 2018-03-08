d5638e7ad594e43d4e499c2dbd91e761bb2c5565
MaxCH330 <asdfgtrt@gmail.com>
2018-03-08 17:42:54 +0800

    ClassWork

---
+++ classwork01/classwork01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("guess_what?");
+            string ans = Console.ReadLine();
+            
+            if (ans == "BANG")
+            {
+                Console.WriteLine("BANG");
+            }
+            Console.Read();
+        }
+    }
+}
